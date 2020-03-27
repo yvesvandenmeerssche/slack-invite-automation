0fe952c605f52c5957679af2284aac214d4d5da8
Yves Van den Meerssche <45092543+yvesvandenmeerssche@users.noreply.github.com>
2020-03-27 10:06:01 +0100

    Update

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // your community or team name to display on join page.
-  community: process.env.COMMUNITY_NAME || 'LTL Maps',
+  community: process.env.COMMUNITY_NAME || 'Join LTL Maps on Slack',
   // your slack team url (ex: socketio.slack.com)
   slackUrl: process.env.SLACK_URL || 'ltlmaps.slack.com',
   // access token of slack
@@ -12,10 +12,10 @@ module.exports = {
   //   --compressed
   slacktoken: process.env.SLACK_TOKEN || 'YOUR-ACCESS-TOKEN',
   // an optional security measure - if it is set, then that token will be required to get invited.
-  inviteToken: process.env.INVITE_TOKEN || null,
+  // inviteToken: process.env.INVITE_TOKEN || null,
   // an optional security measure - if both are set, then recaptcha will be used.
-  recaptchaSiteKey: process.env.RECAPTCHA_SITE || null,
-  recaptchaSecretKey: process.env.RECAPTCHA_SECRET || null,
+  //recaptchaSiteKey: process.env.RECAPTCHA_SITE || null,
+  //recaptchaSecretKey: process.env.RECAPTCHA_SECRET || null,
   // default locale
   locale: process.env.LOCALE || "en",
   subpath: process.env.SUBPATH || "/",
